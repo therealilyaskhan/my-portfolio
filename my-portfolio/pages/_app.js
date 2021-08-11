8e10f99e257969760ceb0cbdc6da708458393405
therealilyaskhan <therealilyaskhan@hotmail.com>
2021-08-11 05:01:03 +0500

    aos fixed by chaning the startEvent option from DOMContentLoaded document event to ---> load event on the document node

--- my-portfolio/pages/_app.js
+++ my-portfolio/pages/_app.js
@@ -10,7 +10,7 @@ function MyApp({ Component, pageProps }) {
     AOS.init({
       // Global settings:
       disable: false, // accepts following values: 'phone', 'tablet', 'mobile', boolean, expression or function
-      startEvent: 'DOMContentLoaded', // name of the event dispatched on the document, that AOS should initialize on
+      startEvent: 'load', // name of the event dispatched on the document, that AOS should initialize on
       initClassName: 'aos-init', // class applied after initialization
       animatedClassName: 'aos-animate', // class applied on animation
       useClassNames: false, // if true, will add content of `data-aos` as classes on scroll
@@ -28,6 +28,7 @@ function MyApp({ Component, pageProps }) {
       anchorPlacement: 'top-bottom', // defines which position of the element regarding to window should trigger the animation
     });
 
+    AOS.refresh();
   }, []);
 
   return <Component {...pageProps} />;
